c9ebeebacbc391725a1f31de2221a3c451363b6d
Mahdi-cmd <66637075+Mahdi-cmd@users.noreply.github.com>
2020-07-04 17:14:10 +0300

    f

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -7,11 +7,26 @@
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
 
-function greet(name) {}
+function greet(name) {
+ 
+}
 
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+function isOdd(n) {
+   
+    if(n%2==1){
+        console.log("odd")
+    }
+    else if(n %2 == 0){
+        console.log("even")
+    }
+    else{
+        console.log("Wrong number")
+    }
+}
+
+isOdd(5);
